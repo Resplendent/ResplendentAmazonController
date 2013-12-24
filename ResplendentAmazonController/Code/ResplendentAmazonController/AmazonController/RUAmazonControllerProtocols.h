92b1f4e7e8c590fbbbec39ae27b370585babb0ec
BenMaer <ben@resplendent.co>
2013-12-23 20:00:48 -0500

    finished

---
+++ ResplendentAmazonController/Code/ResplendentAmazonController/AmazonController/RUAmazonControllerProtocols.h
@@ -0,0 +1,19 @@
+//
+//  RUAmazonControllerProtocols.h
+//  Pineapple
+//
+//  Created by Benjamin Maer on 11/12/13.
+//  Copyright (c) 2013 Pineapple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class RACAmazonController;
+@class AmazonServiceResponse;
+
+@protocol RUAmazonControllerDelegate <NSObject>
+
+-(void)amazonController:(RACAmazonController*)amazonController didFinishWithResponse:(AmazonServiceResponse *)response;
+-(void)amazonController:(RACAmazonController*)amazonController didFailWithError:(NSError*)error;
+
+@end
---
+++ ResplendentAmazonController/RACAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RACAppDelegate.h
+//  ResplendentAmazonController
+//
+//  Created by Benjamin Maer on 12/23/13.
+//  Copyright (c) 2013 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RACAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
